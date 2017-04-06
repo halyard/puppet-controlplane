6462f5d7ecb36b3e1b8638eaf3cbe2be6662ce59
Les Aker <me@lesaker.org>
2017-04-05 23:31:52 -0400

    fix linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,12 +7,12 @@ class controlplane (
 ) {
   package { 'controlplane':
     provider => 'brewcask'
-  } ->
-  osx_login_item { 'ControlPlane':
+  }
+  -> osx_login_item { 'ControlPlane':
     hidden => true,
     path   => $install_path
-  } ~>
-  exec { 'launch controlplane':
+  }
+  ~> exec { 'launch controlplane':
     command     => "/usr/bin/open ${install_path}",
     refreshonly => true,
   }
